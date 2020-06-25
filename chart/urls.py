e765caf00ac42051c12aa1dcbe30ed2f8e9d65dc
kanghw1103 <kanghw1103@naver.com>
2020-06-25 20:42:47 +0900

    add static_root

--- chart/urls.py
+++ chart/urls.py
@@ -2,15 +2,18 @@
 
 from django.contrib import admin
 from django.urls import path
-from chart import views                                     # !!!
+from chart import views
+
+from .views import *# !!!
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('ticket-class/2/',
-         views.ticket_class_view_2, name='ticket_class_view_2'),
+    path('covid19/',
+         covid19_view, name='covid19'),
     path('ticket-class/3/',
          views.ticket_class_view_3, name='ticket_class_view_3'),
     path('json-example/', views.json_example, name='json_example'),
     path('json-example/data/', views.chart_data, name='chart_data'),
     path('admin/', admin.site.urls),
+    # path('covid19new/', covid19_view_new, name='covid19new'),
 ]
--- chart/views.py
+++ chart/views.py
@@ -5,9 +5,7 @@ from django.db.models import Count, Q
 import json
 from django.http import JsonResponse
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter
-import matplotlib.ticker as ticker
+import arrow
 
 
 def home(request):
@@ -30,104 +28,87 @@ def ticket_class_view_1(request):  # 방법 1
     return render(request, 'ticket_class_1.html', {'dataset': dataset})
 
 
-def ticket_class_view_2(request):  # 방법 2
-    df = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv', parse_dates=['Date'])
+def covid19_view(request):  # 방법 2
+    # Section 2 - Loading and Selecting Data
+    df = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv',
+                     parse_dates=['Date'])
+
     countries = ['Korea, South', 'Germany', 'United Kingdom', 'US', 'France']
     df = df[df['Country'].isin(countries)]
-    df['Cases'] = df[['Confirmed', 'Recovered', 'Deaths']].sum(axis=1)
+
+    df['Cases'] = df[['Confirmed']].sum(axis=1)
+
     df = df.pivot(index='Date', columns='Country', values='Cases')
+
     countries = list(df.columns)
+    # df.reset_index()를 통하여 기존 인덱스 열을 데이터 열로 변경
     covid = df.reset_index('Date')
+    # covid 인덱스와 columns를 새로 지정
     covid.set_index(['Date'], inplace=True)
     covid.columns = countries
+    # print(covid.head())
+
+
     populations = {'Korea, South': 51269185, 'Germany': 83783942, 'United Kingdom': 67886011, 'US': 331002651,
                    'France': 65273511}
     percapita = covid.copy()
     for country in list(percapita.columns):
         percapita[country] = percapita[country] / populations[country] * 1000000
 
-    colors = {'Korea, South': '#045275', 'France': '#7CCBA2', 'Germany': '#FCDE9C', 'US': '#DC3977',
-              'United Kingdom': '#7C1D6F'}
-    plt.style.use('fivethirtyeight')
-    # 원 코드의 선과 레이블 색상이 맞지 않는 문제를 해결한 코드
-    # for country in countries:
-    #     plot = percapita[country].plot(figsize=(12, 8), color=colors[country], linewidth=5, legend=False)
-    #
-    # plot.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
-    # plot.grid(color='#d4d4d4')
-    # plot.set_xlabel('Date')
-    # plot.set_ylabel('# of Cases per 1,000,000 People')
-    # for country in list(colors.keys()):
-    #     plot.text(x=covid.index[-1], y=covid[country].max(),
-    #               color=colors[country], s=country, weight='bold')
+    my_data = list()
+    for country in list(percapita.columns):
+        my_series = list()
+        for d in percapita.index.tolist():
+            my_series.append([arrow.get(d.year, d.month, d.day).timestamp * 1000, round(percapita.loc[d][country], 1)])
 
+        my_dict = dict()
+        my_dict['country'] = country
+        my_dict['series'] = my_series
+        my_data.append(my_dict)
 
-    # 빈 리스트 5종 준비
-    france_data = list()
-    germany_data = list()
-    korea_South_data = list()
-    us_data = list()
-    united_Kingdom_data = list()
+    print(list(map(
+        lambda entry: {'name': entry['country'], 'data': entry['series']},
+        my_data)))
 
-
-    # 리스트 5종에 형식화된 값을 등록
-    for entry in percapita.index:
-        france_data.append(entry["France"])
-        germany_data.append(entry['Germany'])
-        korea_South_data.append(entry['Korea, South'])
-        us_data.append(entry['US'])
-        united_Kingdom_data.append(entry['United Kingdom'])
-
-
-    France = {
-        'name': 'France',
-        "type": "spline",
-        "yAxis": 1,
-        'data': france_data,
-        'color': 'green',
-    }
-
-    Germany = {
-        'name': 'Germany',
-        "type": "spline",
-        "yAxis": 1,
-        'data': germany_data,
-        'color': 'red',
-    }
-
-    Korea_South = {
-        'name': 'Korea_South',
-        'type': 'spline',
-        'data': korea_South_data,
-        'color': 'skyblue',
-    }
-
-    US = {
-        'name': 'US',
-        'type': 'spline',
-        'data': us_data,
-        'color': 'skyblue',
-    }
-
-    United_Kingdom = {
-        'name': 'United_Kingdom',
-        'type': 'spline',
-        'data': united_Kingdom_data,
-        'color': 'skyblue',
-    }
 
     chart = {
-        'chart': {'type': 'spline'},
+        'chart': {
+            'type': 'spline',
+            'borderColor': '#9DB0AC',
+            'borderWidth': 3,
+        },
         'title': {'text': 'COVID-19 확진자 발생율'},
-        'xAxis': {'type': "datetime"},
-        "yAxis": [{"labels": {"format": "{value} 건/백만 명", "style": {"color": "blue"}},
-                   "title": {"text": "합계건수", "style": {"color": "blue"}}}],
-        "tooltip": {"shared": "true"},
-        'series': [France, Germany, Korea_South, US, United_Kingdom]
+        'subtitle': {'text': 'Source: Johns Hopkins University Center for Systems Science and Engineering'},
+        'xAxis': {'type': 'datetime',
+                  },
+        'yAxis': [{
+            'labels': {
+                'format': '{value} 건/백만 명',
+                'style': {'color': 'blue'}
+            }, 'title': {
+                'text': '합계 건수',
+                'style': {'color': 'blue'}
+            },
+        }, ],
+        'plotOptions': {
+            'spline': {
+                'lineWidth': 3,
+                'states': {
+                    'hover': {'lineWidth': 5}
+                },
+            }
+        },
+        'series': list(map(
+            lambda entry: {'name': entry['country'], 'data': entry['series']},
+            my_data)
+        ),
+        'navigation': {
+            'menuItemStyle': {'fontSize': '10px'}
+        },
     }
     dump = json.dumps(chart)
+    return render(request, 'covid19.html', {'chart': dump})
 
-    return render(request, 'ticket_class_2.html', {'chart': dump})
 
 
 def ticket_class_view_3(request):  # 방법 3
